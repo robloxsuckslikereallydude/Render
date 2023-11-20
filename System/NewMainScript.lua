120420d26ff9c2f8711bcb36c0978514bf0da6cd
VoidCustomer <94419318+SystemXVoid@users.noreply.github.com>
2023-11-19 20:31:22 -0500

    Update NewMainScript.lua

--- System/NewMainScript.lua
+++ System/NewMainScript.lua
@@ -4,8 +4,12 @@ end
 
 local writefile = writefile or function() return "" end
 local isfolder = isfolder or function() return false end
+local makefolder = makefolder or function() end
 
 local function getvapefile(file)
+	if not isfolder("vape") then 
+		makefolder("vape")
+	end
 	if not isfile("vape/"..file) then 
 		local custom = {"MainScript.lua", "Universal.lua", "GuiLibrary.lua"}
 		local success, script = pcall(function()
@@ -13,12 +17,10 @@ local function getvapefile(file)
 			return game:HttpGet("https://raw.githubusercontent.com/"..url)
 		end)
 		if success and script ~= "404: Not Found" then 
-			if isfolder("vape") then 
-				if file:sub(#file - 4, #file) == ".lua" and custom[file] then 
-					script = ("Render Custom Vape Signed File\n"..script)
-				end
-				writefile("vape/"..file, script)
+			if file:sub(#file - 4, #file) == ".lua" and custom[file] then 
+				script = ("Render Custom Vape Signed File\n"..script)
 			end
+			writefile("vape/"..file, script)
 		else
 			task.spawn(error, "Vape - Failed to download\n vape/"..file..". | "..(script or "404: Not Found"))
 		end
@@ -31,5 +33,4 @@ if isfolder("vape") and not isfile("vape/commithash.txt") then
 	writefile("vape/commithash.txt")
 end
 
-print("loaded")
 loadstring(getvapefile("MainScript.lua"))()
