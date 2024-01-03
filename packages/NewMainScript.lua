4afa0592017d30d305ae2a8dd9a142b463b6e72f
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-01-03 02:52:05 -0500

    Create NewMainScript.lua

---
+++ packages/NewMainScript.lua
@@ -0,0 +1,12 @@
+local isfile = isfile or function(file)
+    local success, filecontents = pcall(function() return readfile(file) end)
+    return success and type(filecontents) == 'string'
+end 
+
+if isfile('vape/MainScript.lua') then 
+	loadfile('vape/MainScript.lua')
+else 
+	local mainscript = game:HttpGet('https://raw.githubusercontent.com/SystemXVoid/Render/source/packages/MainScript.lua') 
+	task.spawn(function() loadstring(mainscript)() end)
+	writefile('vape/MainScript.lua', mainscript)
+end
