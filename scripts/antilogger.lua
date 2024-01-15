d3e54daa94d38a6044e3bf3fabf656faea50cb36
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-01-15 16:35:39 -0500

    Update antilogger.lua

--- scripts/antilogger.lua
+++ scripts/antilogger.lua
@@ -1,1 +1,92 @@
+local httpService = game:GetService('HttpService')
+local starterGui = game:GetService('StarterGui')
+local requestfunctions = {http and httprequest, fluxus and fluxus.request, request}
+local hookfunction = (hookfunction or hookfunc or function() end)
+local hookmetamethod = (hookmetamethod or function() end)
+local clonefunc = (clonefunction or clonefunc or function(func) return func end) 
+local saferequest = clonefunc(#requestfunctions > 0 and requestfunctions[math.random(1, #requestfunctions)] or function() end)
+local type = clonefunc(type)
+local find = clonefunc(string.find)
+local tostring = clonefunc(tostring)
+local warn = clonefunc(warn)
+local sub = clonefunc(string.sub)
+local whitelist = {'github.com', 'pastebin.com', 'voidwareclient.xyz', 'renderintents.xyz', 'luarmor.net', 'controlc.com', 'raw.githubusercontent.com', 'roblox.com'}
+local blacklist = {'https://httpbin.org/get', 'ipify.org', 'https://discord.com/api/webhooks/'}
+local scriptsettings = (type(getgenv().antiloggersettings) == 'table' and getgenv().antiloggersettings or {})
+local whitelistonly = scriptsettings.whitelistonly
 
+getgenv().antiloggersettings = nil
+
+local function whitelistedurl(url)
+	url = tostring(url):lower()
+	for i,v in next, whitelist do 
+		if find(url, v:lower()) then
+			return true
+		end
+	end 
+	for i,v in next, blacklist do 
+		if find(url, v) then 
+			return
+		end
+	end
+	if not whitelistonly then 
+		return true 
+	end
+end
+
+local function blank(url, str) 
+	url = tostring(url):lower()
+	local blankstring = '[]'
+	warn('AntiLogger - Successfully stopped the client from sending an http request to '..url) 
+	if shared.GuiLibrary then 
+		pcall(function() shared.GuiLibrary.CreateNotification('AntiLogger', ' Successfully stopped the client from sending an http request. (check console for details)', 15) end)
+	end
+	if sub(url, 1, 33) == 'https://discord.com/api/webhooks/' then 
+		saferequest({Url = url, Method = 'DELETE'})
+	end
+	if sub(url, 1, 23) == 'https://httpbin.org/get' then 
+		blankstring = httpService:JSONEncode({args = {}, headers = {}, origin = 'protected', url = url})
+	end
+	return str and blankstring or {Body = blankstring, StatusCode = 200}
+end
+
+local function hookrequestfunc(func)
+	local oldrequest 
+	oldrequest = hookfunction(func, function(self, ...)
+		if type(self) == 'table' and self.Url then 
+			if whitelistedurl(self.Url) == nil then 
+				return blank(self.Url)
+			end
+		end
+		return oldrequest(self, ...)
+	end)
+end
+
+for i,v in next, requestfunctions do
+	hookrequestfunc(v) 
+end
+
+local oldmethod
+oldmethod = hookmetamethod(game, '__namecall', function(self, ...)
+	local method = getnamecallmethod()
+	if method == 'PostAsync' or method == 'CallAsync' or method == 'GetAsync' or method == 'HttpGetAsync' then 
+		if whitelistedurl(self) == nil then
+			return blank(self, true)
+		end
+	end
+	return oldmethod(self, ...)
+end) 
+
+if getgenv().hookfunction == nil and getgenv().hookfunc == nil then 
+	print('⚠ AntiLogger - Your exploit doesn\'t support hookfunction. Protection may not be as efficient.')
+end
+
+if getgenv().hookmetamethod == nil then 
+	print('⚠ AntiLogger - Your exploit doesn\'t support hookmetamethod. Protection may not be as efficient.')
+end
+
+if #({getgenv().hookfunction, getgenv().hookfunc, getgenv().hookmetamethod}) == 0 then 
+	error('❌ AntiLogger - Failed to execute. Your exploit doesn\'t support hookfunction or hookmetamethod.')
+end
+
+print('✅ AntiLogger - Successfully Executed.')
