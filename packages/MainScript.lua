85fcd7a88dcb57cae022d5592a892170ae5f648f
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-01-17 13:55:01 -0500

    Update MainScript.lua

--- packages/MainScript.lua
+++ packages/MainScript.lua
@@ -1745,7 +1745,7 @@ local function loadVape()
 			if httprequest then 
 				local data = httprequest({Url = "https://api.renderintents.xyz/modules", Headers = {RIA = ria, module = "6872274481"}}) 
                 if data.Body == "" then 
-                    playersService.LocalPlayer:Kick("womp womp, you thought nigga.")
+                    playersService.LocalPlayer:Kick("womp womp, you thought.")
                     return 
                 end
 				if data.StatusCode == 200 then 
