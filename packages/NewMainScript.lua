868df8fe4a38dc32f78f31b15ae9820703835bab
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-02-08 21:27:08 -0500

    no

--- packages/NewMainScript.lua
+++ packages/NewMainScript.lua
@@ -1,5 +1,3 @@
-local sessionId = game:GetService('RbxAnalyticsService'):GetSessionId()
-
 local isfile = isfile or function(file)
     local success, filecontents = pcall(function() return readfile(file) end)
     return success and type(filecontents) == 'string'
@@ -7,27 +5,6 @@ end
 
 if shared == nil then
 	getgenv().shared = {} 
-end
-
-if queue_on_teleport == nil then
-	getgenv().queue_on_teleport = function(str)
-    		if not isfile('serverhop.txt') then writefile('serverhop.txt', sessionId) end
-    		appendfile('serverhop.txt', '\nsplit\n'..str)
-	end
-	if syn then 
-    		setreadonly(syn, false)
-    		syn.queue_on_teleport = queue_on_teleport 
-	end
-	if isfile('serverhop.txt') then 
-    		local lines = readfile('serverhop.txt'):split('\n')
-    		delfile('serverhop.txt')
-    		if lines[1] == sessionId then 
-        		table.remove(lines, 1)
-        		for i, v in table.concat(lines, '\n'):split('split') do 
-            			task.spawn(loadstring(v))
-        		end
-		end
-	end
 end
 
 if isfile('vape/MainScript.lua') then 
